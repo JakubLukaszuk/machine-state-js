51292ec51e32daf0016da8468b3dc12ff0a4cfd2
JakubLukaszukMediacreations <79305498+JakubLukaszukMediacreations@users.noreply.github.com>
2021-04-01 16:30:24 +0200

    servi8ces added and auto play

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import { createMachine } from 'xstate';
 import { useMachine } from '@xstate/react';
 import React from 'react'
 
-const playerMachine = (ref) => createMachine({
+const playerMachine = (ref, {autoplay} = {autoplay: false}) => createMachine({
   id: "player",
-  initial: "paused",
+  initial: "unknown",
   context: {ref},
   states: {
+    unknown: {
+      always: [
+        {
+          target: "playing",
+          cond: "shouldAutoPlay"
+        },
+        {
+          target: "paused"
+        }
+      ]
+    },
     paused: {
       entry: "stop",
       on: {
@@ -24,6 +35,9 @@ const playerMachine = (ref) => createMachine({
   },
 },
   {
+    guards: {
+      shouldAutoPlay: () => autoplay
+    },
     actions: {
       play: (context) => {
         console.log("PLAY");
@@ -39,22 +53,75 @@ const playerMachine = (ref) => createMachine({
           context.ref.current.pause()
         }
       }
-
     }
   }
 )
 
+const fetchMachine = createMachine({
+  id: "fetch",
+  initial: "idle",
+  on:{
+    FETCH: {
+      target: "pending"
+    }
+  },
+  states: {
+    idle: {},
+    pending: {
+      on: {
+        FETCH: undefined
+      },
+      invoke: {
+        src: "fetch",
+        onDone: {
+          target: "success"
+        },
+        onError: {
+          target: "error"
+        }
+      }
+    },
+    success: {},
+    error: {}
+  }
+},
+{
+  services: {
+    fetch: async () => {
+      return new Promise((resolve, reject)=> {
+        setTimeout(() => {
+          const random = Math.random()
+
+          if(random> 0.5)
+          {
+            resolve()
+          }
+          else
+          {
+            reject()
+          }
+        }, 1000)
+      })
+    }
+  }
+});
+
 function App() {
   const ref = React.useRef(null);
-  const machine = playerMachine(ref);
+  const machine = playerMachine(ref, {autoplay: true});
   const [current, send] = useMachine(machine);
+  const [currentSrvice, sendService] = useMachine(fetchMachine);
   console.log(current);
   return (
     <div>
-    <video ref={ref} src="https://cdn.videvo.net/videvo_files/video/premium/video0293/small_watermarked/_TeaserNewYear109_preview.webm" />
+      <video ref={ref} src="https://cdn.videvo.net/videvo_files/video/premium/video0293/small_watermarked/_TeaserNewYear109_preview.webm" />
       <br/>
       <button onClick={() => send("STOP")}>STOP</button>
       <button onClick={() => send("PLAY")}>PLAY</button>
+      <br/>
+      <br/>
+      {currentSrvice.value}
+      <button onClick={() => sendService("FETCH")}>fetch</button>
     </div>
   );
 }
