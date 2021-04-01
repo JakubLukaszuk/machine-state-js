777766ce6e191787af586d781cf071ccef8da8d1
JakubLukaszukMediacreations <79305498+JakubLukaszukMediacreations@users.noreply.github.com>
2021-04-01 10:46:41 +0200

    add real video

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { createMachine } from 'xstate';
 import { useMachine } from '@xstate/react';
+import React from 'react'
 
-const playerMachine = createMachine({
+const playerMachine = (ref) => createMachine({
   id: "player",
   initial: "paused",
+  context: {ref},
   states: {
     paused: {
       entry: "stop",
@@ -23,18 +25,34 @@ const playerMachine = createMachine({
 },
   {
     actions: {
-      play: () => { console.log("PLAY"); },
-      stop: () => { console.log("STOP"); }
+      play: (context) => {
+        console.log("PLAY");
+        if(context.ref.current)
+        {
+          context.ref.current.play()
+        }
+      },
+      stop: (context) => {
+        console.log("STOP");
+        if(context.ref.current)
+        {
+          context.ref.current.pause()
+        }
+      }
 
     }
   }
 )
 
 function App() {
-  const [current, send] = useMachine(playerMachine);
+  const ref = React.useRef(null);
+  const machine = playerMachine(ref);
+  const [current, send] = useMachine(machine);
   console.log(current);
   return (
     <div>
+    <video ref={ref} src="https://cdn.videvo.net/videvo_files/video/premium/video0293/small_watermarked/_TeaserNewYear109_preview.webm" />
+      <br/>
       <button onClick={() => send("STOP")}>STOP</button>
       <button onClick={() => send("PLAY")}>PLAY</button>
     </div>
